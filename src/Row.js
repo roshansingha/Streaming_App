7da79c409b8ee8aa32c33dc28ff51fe7be473e7a
Roshan Singha <roshansinghaagt@gmail.com>
2020-08-05 23:37:35 +0530

    netlify

--- src/Row.js
+++ src/Row.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "./axios";
 import "./Row.css";
-import YouTube from "react-youtube";
 
 const base_url = "https://image.tmdb.org/t/p/original/";
 
@@ -32,7 +31,6 @@ function Row({ title, fetchUrl, isLargeRow }) {
           />
         ))}
       </div>
-      <YouTube videoId={trailerUrl} opts={opts} />
     </div>
   );
 }
